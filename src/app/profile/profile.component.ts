639e2a9ad10b4f7dccf24bcb58c14a1992750186
jakkulasairam <sairam.jakkula@kodemtechnologies.com>
2022-07-14 16:47:30 +0530

    profile added

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-profile',
@@ -8,10 +9,13 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class ProfileComponent implements OnInit {
   username: string="";
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute,private http:HttpClient) { }
   ngOnInit() {
     this.username=this.route.snapshot.params['email'];
     console.log(this.username);
+    this.http.get('http://localhost/user/'+this.username).subscribe(response=>{
+      console.log(response);
+    })
   }
 
 }
